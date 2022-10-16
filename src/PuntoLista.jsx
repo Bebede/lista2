1fe777859f88523795cadc9cfdf61696ac273320
bebede <pokebenny2@gmail.com>
2022-10-16 15:55:30 +0200

    opera

--- src/PuntoLista.jsx
+++ src/PuntoLista.jsx
@@ -15,8 +15,10 @@ function PuntoLista() {
                 </div>
                 </div>
                 <div className="niente"></div>
-                <h2 className="titolopunto"></h2>
-                <p className="descrizionepunto"></p>
+                <h2 className="titolopunto"> struttura scolastica </h2>
+                <p className="descrizionepunto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati fugit amet illum minima deserunt animi corporis excepturi culpa assumenda? Voluptatibus tempora assumenda iste explicabo corporis accusamus fugit repellendus quidem!
+                Totam esse mollitia unde optio debitis repellendus architecto veniam voluptas modi hic laboriosam nobis doloribus praesentium veritatis repudiandae labore, id commodi iure aperiam aut obcaecati nulla quibusdam, facere nesciunt? Accusamus?
+                i,  tenetur nisi hic facilis obcaecati quaerat dolorum saepe accusantium aspernatur. Distinctio, eius!</p>
             </div>
             <div className="unpunto">
                 <div className="linee">
